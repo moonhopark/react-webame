69c4e1c6bf86234a1ac5f300402e225fa2bdb75d
unknown <12171928@inha.edu>
2021-01-04 13:14:51 +0900

    웹팩 초기 시작세팅 중

---
+++ 2.끝말잇기/WordRelay.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+const { JavascriptModulesPlugin } = require('webpack');
+const { Component } = React;
+// 사용하는 패키지 같은 거를 불러옴
+
+class WordRelay extends React.Component {
+  state  = {
+
+  };
+
+  render() {
+
+  };
+}
+
+module.exports = WordRelay;
+// 이 component를 다른데서 사용할 수 있게 해줌
---
+++ 2.끝말잇기/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const WordRealy = requires('./WordRelay')
+
+ReactDom.render(<WordRealy />, document.querySelector('#root'))
