784364d4d8e01ce8dec3d0a88cd65aea07f8a2ca
unknown <12171928@inha.edu>
2021-01-04 15:03:33 +0900

    웹팩으로 빌드하는 방법 기초

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,3 +1,31 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+  name: 'wordrelay-settings',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval', // 빠르게
+  resolve: {
+    extensions: ['.js','.jsx'],
+  }, // entry안에 파일 확장자를 없애는 대신 resolve를 추가하면 알아서 파일 찾아줌.
+  
+  entry: {
+    app: ['./client'], // client.jsx에서 WordRelay.jsx를 불러와서 여기에 안 써줘도 된다.
+  }, // entry에 있는 파일을 읽고
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env','@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    }],
+  }, // module을 적용
+
+
+  output: {
+    path: path.join(__dirname, 'dist'), // path.join 하면 dirname(현재폴더)에 'dist'경로 연결해줌
+    filename: 'app.js'
+  }, // output으로 출력
 };
