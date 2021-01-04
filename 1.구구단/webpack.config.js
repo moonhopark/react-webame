4f08603d493d902669b787b5b3ed979abab40074
unknown <12171928@inha.edu>
2021-01-04 16:59:59 +0900

    수정

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+<<<<<<< HEAD
 const { webpack } = require('webpack');
+=======
+>>>>>>> df57a4d (cache clear)
 
 module.exports = {
   mode: 'development',
@@ -16,6 +19,7 @@ module.exports = {
       test: /\jsx?$/,
       loader: 'babel-loader',
       options: {
+<<<<<<< HEAD
         presets: [
           ['@babel/preset-env', {
             targets: {
@@ -23,17 +27,27 @@ module.exports = {
             },
           }],
           '@babel/preset-react'],
+=======
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+>>>>>>> df57a4d (cache clear)
         plugins: [],
       },
     }],
   },
+<<<<<<< HEAD
   plugins: [
   ],
+=======
+>>>>>>> df57a4d (cache clear)
   output: {
     filename: 'app.js',
     path: path.join(__dirname, 'dist'),
   },
+<<<<<<< HEAD
 }
 
 //코드 작성할 때 위 순서대로 작성하기
 //entry에 있는 파일에 module을 적용하고 plugin까지 한번더 적용후 output으로 나온다.
+=======
+}
+>>>>>>> df57a4d (cache clear)
