df57a4d49a87e0e7c253f2453a0a9b3cfc868370
unknown <12171928@inha.edu>
2021-01-04 15:34:07 +0900

    cache clear

---
+++ 1.구구단/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval', // 배포할 때 hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  entry: {
+    app: './client',
+  },
+  module:{
+    rules:[{
+      test: /\jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: [],
+      },
+    }],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+  },
+}
