b5016cbda111f804f176efddd1fa1608169dbc21
moonhopark <12171928@inha.edu>
2021-01-06 14:17:37 +0900

    React 조건문을 이용 renderAverage 구현

--- 4.ResponseCheck/ResponseCheck.jsx
+++ 4.ResponseCheck/ResponseCheck.jsx
@@ -7,16 +7,29 @@ class ResponseCheck extends Component {
     result: [],
   };
 
+  OnClickScreen = () => {
+
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    return result.length === 0
+      ? null
+      : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+  }
+
   render() {
+    const { state, message }= this.state;
     return(
       <>
         <div
           id="screen"
-          className={this.state.state}
+          className={state}
           onClick={this.onClickScreen}
         >
+          {message}
         </div>
-        <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+        {this.renderAverage()}
       </>
     )
   }
