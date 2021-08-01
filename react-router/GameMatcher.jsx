689d61aaa916b113e1ad767bb0fcd2d124b5e086
moonho park <moonho4199@naver.com>
2021-08-01 13:52:21 +0900

    동적라우팅 매칭

--- react-router/GameMatcher.jsx
+++ react-router/GameMatcher.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
+import NumberBaseball from '../3.NumberBaseball/NumberBaseball';
+import RSP from '../5.RSP/RSP';
+import Lotto from '../6.Lotto/Lotto';
+
 class GameMatcher extends Component {
   render() {
-    console.log(this.props);
-    return <div>게임매쳐</div>;
+    if (this.props.match.params.name === 'number-baseball') {
+      return <NumberBaseball />;
+    } else if (this.props.match.params.name === 'rock-scissors-paper') {
+      return <RSP />;
+    } else if (this.props.match.params.name === 'lotto-generator') {
+      return <Lotto />;
+    }
+    return <div>일치하는 게임이 없습니다.</div>;
   }
 }
 
